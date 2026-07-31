bb401e2104a370a149370cafe030699ca4656482
SarthakBitbyBit <sarthakisworkingonline@gmail.com>
2026-07-31 22:32:51 +0530

    Add Assignment 10

---
+++ assignments/assignment10/Assignment10.java
@@ -0,0 +1,30 @@
+abstract class Payment {
+    abstract void pay(double amount);
+
+    void receipt() {
+        System.out.println("Payment receipt generated.");
+    }
+}
+
+class CreditCardPayment extends Payment {
+    void pay(double amount) {
+        System.out.println("Paid by credit card: " + amount);
+    }
+}
+
+class UpiPayment extends Payment {
+    void pay(double amount) {
+        System.out.println("Paid by UPI: " + amount);
+    }
+}
+
+public class Assignment10 {
+    public static void main(String[] args) {
+        Payment card = new CreditCardPayment();
+        Payment upi = new UpiPayment();
+        card.pay(1200);
+        card.receipt();
+        upi.pay(850);
+        upi.receipt();
+    }
+}
