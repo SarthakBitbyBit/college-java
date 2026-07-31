81cfeba24e653bb0504077b62030976233023c46
SarthakBitbyBit <sarthakisworkingonline@gmail.com>
2026-07-31 22:32:51 +0530

    Add Assignment 6

---
+++ assignments/assignment06/Assignment06.java
@@ -0,0 +1,31 @@
+interface DeliveryAction {
+    void update(String message);
+}
+
+class Vehicle {
+    private String number = "KA-01-1234";
+
+    class Details {
+        void display() {
+            System.out.println("Vehicle number: " + number);
+        }
+    }
+
+    void startDelivery() {
+        DeliveryAction action = new DeliveryAction() {
+            public void update(String message) {
+                System.out.println("Delivery status: " + message);
+            }
+        };
+        action.update("Started");
+    }
+}
+
+public class Assignment06 {
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        Vehicle.Details details = vehicle.new Details();
+        details.display();
+        vehicle.startDelivery();
+    }
+}
